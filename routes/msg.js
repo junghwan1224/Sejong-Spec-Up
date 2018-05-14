33ff4cb7e16bf65bfddf5fe5bcdb574b131136b5
Ubuntu <ubuntu@ip-172-31-21-14.ap-northeast-2.compute.internal>
2018-05-14 18:54:04 +0000

    메세지 기본 기능

---
+++ routes/msg.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var dbconfig = require('../database.js');
+var conn = mysql.createConnection(dbconfig);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var sql = 'select * from msg where recv_name=?';
+  if(req.session.authId){
+    conn.query(sql, [req.session.authId], function(error, results){
+      if(error){
+        console.log(error);
+        console.log('쪽지 리스트 나열 실패');
+      }
+      else{
+        res.render('msg_test',{
+          user: req.session.authId,
+          msgList: results
+        });
+      }
+    });
+  }
+  else
+    res.render('login',{
+      title:'openyearround', // 사이트 제목
+    });//render
+});
+
+router.post('/postMsg', function(req, res){
+  var sql = 'insert into `msg` (`post_name`, `recv_name`, `title`, `contents`, `date`) values (?, ?, ?, ?, ?);';
+  var post_name = req.body.post_user;
+  var recv_name = req.body.recv_user;
+  var title = req.body.title;
+  var contents = req.body.contents;
+  var date = req.body.post_date;
+  conn.query(sql, [post_name, recv_name, title, contents, date], function(error, results){
+    if(error){
+      console.log(error);
+      console.log('쪽지 전송 실패');
+    }
+    else{
+      console.log('쪽지 전송 성공');
+      res.send({result: 'success'});
+    }
+  });
+});
+
+router.get('/msgDetail/:id', function(req, res){
+  var sql = 'select * from msg where id=? and recv_name=?;';
+  var update_sql = 'update `msg` set `check`=? where `id`=? and `recv_name`=?;';
+  var id = req.params.id;
+  conn.query(sql, [id, req.session.authId], function(error, results){
+    if(error){
+      console.log(error);
+      console.log('msgDetail select 쿼리문 에러');
+    }
+    else{
+      console.log('msgDetail select 쿼리문 성공');
+      conn.query(update_sql, [1, parseInt(id), req.session.authId], function(error, rows){
+        if(error){
+          console.log(error);
+          console.log('update sql 쿼리문 실패');
+        }
+        else{
+          console.log('update sql 쿼리문 성공');
+          res.render('msg_detail', {
+            title: results[0].title,
+            date: results[0].date,
+            post_name: results[0].post_name,
+            contents: results[0].contents,
+          });
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
