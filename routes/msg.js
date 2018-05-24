d5a8041e4fe1dda2805840bd49c853242f23e6e7
Ubuntu <ubuntu@ip-172-31-21-14.ap-northeast-2.compute.internal>
2018-05-24 06:07:21 +0000

    잠시확인2

--- routes/msg.js
+++ routes/msg.js
@@ -5,10 +5,10 @@ var mysql = require('mysql');
 var dbconfig = require('../database.js');
 var conn = mysql.createConnection(dbconfig);
 
-var http = require('http');
-var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+// var http = require('http');
+// var app = express();
+// var server = http.createServer(app);
+// var io = require('socket.io').listen(server);
 
 // io.on('connection', function(socket){
 //   console.log('connected');
